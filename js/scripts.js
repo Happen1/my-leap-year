bd7eda5ae031ffb5c3c720d3c2f4b7a4a250bd3b
Ohene Gyan <Janmillz@yahoo.com>
2017-09-21 15:23:55 +0100

    if change 100 operator and employing 4872 to make the code shorter

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,13 @@ var leapYear = function(year) {
 };
 
 // jQuery user interface logic here.
+
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// jQuery user interface logic here.
