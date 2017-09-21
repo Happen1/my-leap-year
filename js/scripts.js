be8b59981bdd79c3bc9742ab4c5bcdce61f6a6a2
Ohene Gyan <Janmillz@yahoo.com>
2017-09-21 16:13:21 +0100

    user interface logic completed

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,22 @@ var leapYear = function(year) {
 };
 
 // jQuery user interface logic here.
+
+// user interface logic
+$(document).ready(function() {
+  $("#submit-button").click(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) { // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
