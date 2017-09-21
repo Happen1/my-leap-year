b9ac1add1a43110ad9184d96206ce30fd83bb868
Ohene Gyan <Janmillz@yahoo.com>
2017-09-21 14:59:49 +0100

    addition of years been divisible by 4

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,13 @@ $(document).ready(function() {
 var leapYear = function(year) {
   return false;
 };
+
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// jQuery user interface logic here.
